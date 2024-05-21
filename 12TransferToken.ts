88b7eb89d8702d003ef6c167cbb8a26e1e92525c
DESKTOP-83HNANL\MykhailoZ <chittykitty111149@outlook.com>
2024-05-21 08:13:31 +0800

    complete transfer token from one account to another one

--- 12TransferToken.ts
+++ 12TransferToken.ts
@@ -1,5 +1,43 @@
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 import { generateKeypair } from "./2secretkey2publickey";
+import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import { getExplorerLink } from "@solana-developers/helpers";
+require('dotenv').config()
 
-const connection = new Connection(clusterApiUrl("devnet"))
+const connection = new Connection(clusterApiUrl("devnet"), {
+    commitment: "confirmed"
+})
 const sender = generateKeypair();
+const recipient = new PublicKey(process.env.RECIPIENT_ADDRESS);
+const tokenMintAccount = new PublicKey(process.env.MINT_PUBLIC_KEY);
+const MINOR_UINTS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+const sourceTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    sender,
+    tokenMintAccount,
+    sender.publicKey
+);
+
+const destinationTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    sender,
+    tokenMintAccount,
+    recipient
+);
+
+const signature = await transfer (
+    connection,
+    sender,
+    sourceTokenAccount.address,
+    destinationTokenAccount.address,
+    sender,
+    1 * MINOR_UINTS_PER_MAJOR_UNITS
+);
+
+const explorerLink = getExplorerLink ("transaction", signature, "devnet");
+
+console.log(`✅ Transaction confirmed, explorer link is: ${explorerLink}!`);
+
+// ✅ Transaction confirmed, explorer link is: https://explorer.solana.com/tx/45Kzn1GrWhpY1qsAGUAvqwK8LZPwWJRw87r2E63Ghz6hbWLLhL
+// ANmoZdHP2Py8DGuShfbHfN2cZLE2yz844GRofj?cluster=devnet!
--- 1generate-keypair.ts
+++ 1generate-keypair.ts
@@ -1,9 +1,11 @@
 import { Keypair } from "@solana/web3.js";
 import * as fs from 'fs'
+import { setEnvValue } from "./6updateEnv";
 
 export const generateKeypair = async () => {
     const keypair = Keypair.generate();
-    await fs.writeFileSync('.env', `SECRET_KEY=${keypair.secretKey.toString()}\nPUBLIC_KEY=${keypair.publicKey}`)
+    // await fs.writeFileSync('.env', `SECRET_KEY=${keypair.secretKey.toString()}\nPUBLIC_KEY=${keypair.publicKey}`)
+    setEnvValue("RECIPIENT_ADDRESS", keypair.publicKey)
 }
 
 generateKeypair()
